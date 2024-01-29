503d14cfee6ee2696f11acbce8a0b3020bb3573c
Gabriele Iannetti <g.iannetti@gsi.de>
2024-01-29 13:39:42 +0100

    Add tests

--- collector/pvdisplay.go
+++ collector/pvdisplay.go
@@ -8,7 +8,71 @@
 
 package collector
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
 const (
 	Namespace = "pvdisplay"
 	CliTool   = "pvdisplay"
 )
+
+var (
+	pvdisplayRegex      = regexp.MustCompile(`(?m:^(?:\/dev[\/a-zA-Z0-9\-]+)\s+(?P<vg>[a-zA-Z0-9]+)\s+(?:[a-zA-Z0-9]+)\s+(?:[a-zA-Z\-]+)\s+(?P<psize>[0-9]+)B\s+(?P<pfree>[0-9]+)B$)`)
+	pvdisplayVgIndex    = pvdisplayRegex.SubexpIndex("vg")
+	pvdisplayPSizeIndex = pvdisplayRegex.SubexpIndex("psize")
+	pvdisplayPFreeIndex = pvdisplayRegex.SubexpIndex("pfree")
+)
+
+type PvdisplayItem struct {
+	Vg    string
+	PSize float64
+	PFree float64
+}
+
+func newPvdisplayItem(vg string, psize float64, pfree float64) (*PvdisplayItem, error) {
+	if vg == "" {
+		return nil, fmt.Errorf("vg argument empty")
+	}
+
+	if psize == 0 {
+		return nil, fmt.Errorf("psize is 0")
+	}
+
+	return &PvdisplayItem{vg, psize, pfree}, nil
+}
+
+func ExtractPvdisplayItems(data string) ([]PvdisplayItem, error) {
+	slice := make([]PvdisplayItem, 0, 2)
+	matchedItems := pvdisplayRegex.FindAllStringSubmatch(data, -1)
+
+	if matchedItems == nil {
+		return nil, fmt.Errorf("pvdisplayRegex missmatch on data:\n%s", data)
+	}
+
+	for _, mItem := range matchedItems {
+
+		vg := mItem[pvdisplayVgIndex]
+
+		psize, err := strconv.ParseFloat(mItem[pvdisplayPSizeIndex], 10)
+		if err != nil {
+			return nil, err
+		}
+
+		pfree, err := strconv.ParseFloat(mItem[pvdisplayPFreeIndex], 10)
+		if err != nil {
+			return nil, err
+		}
+
+		pvItem, err := newPvdisplayItem(vg, psize, pfree)
+		if err != nil {
+			return nil, err
+		}
+
+		slice = append(slice, *pvItem)
+	}
+
+	return slice, nil
+}
--- exporter_test.go
+++ exporter_test.go
@@ -10,12 +10,60 @@ package main
 
 import (
 	"testing"
+
+	"prometheus-pvdisplay-exporter/collector"
 )
 
-var (
-	testPminfoFile = "pminfo.txt"
-)
+func TestExtractPvdisplayItems(t *testing.T) {
 
-func TestParsePvdisplayStruct(t *testing.T) {
+	pvdisplayData :=
+		"/dev/disk/by-id/scsi-3600062b20854afc029a65252775956fa mds00vg lvm2 a--  19201016201216B 8205899923456B\n" +
+			"/dev/sdb   mds01vg lvm2 a--  19201016201216B 8205899923456B"
+
+	pvDisplayItems, err := collector.ExtractPvdisplayItems(pvdisplayData)
+	if err != nil {
+		t.Error(err)
+	}
+
+	lenPvdisplayData := len(pvDisplayItems)
+	if lenPvdisplayData != 2 {
+		t.Errorf("Expected 2 items in pvDisplayItems, but got: %d", lenPvdisplayData)
+	}
+
+}
+
+func TestParsePvdisplayItems(t *testing.T) {
+
+	m := make(map[string]collector.PvdisplayItem) // [pvdisplayRawString]expectedPvdisplayItem
+	m["/dev/disk/by-id/scsi-3600062b20854afc029a65252775956fa mds00vg lvm2 a--  19201016201216B 8205899923456B"] = collector.PvdisplayItem{"mds00vg", 19201016201216, 8205899923456}
+	m["/dev/sdb   mds01vg lvm2 a--  19201016201216B 8205899923456B"] = collector.PvdisplayItem{"mds01vg", 19201016201216, 8205899923456}
+
+	for pvdisplayData, expected := range m {
+
+		pvDisplayItems, err := collector.ExtractPvdisplayItems(pvdisplayData)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		lenPvdisplayData := len(pvDisplayItems)
+		if len(pvDisplayItems) != 1 {
+			t.Errorf("Expected 1 item in pvDisplayItems, but got: %d", lenPvdisplayData)
+		}
+
+		pvDisplayItem := pvDisplayItems[0]
+
+		if pvDisplayItem.Vg != expected.Vg {
+			t.Errorf("Expected vg %s, got %s", expected.Vg, pvDisplayItem.Vg)
+		}
+
+		if pvDisplayItem.PSize != expected.PSize {
+			t.Errorf("Expected PSize %f, got %f", expected.PSize, pvDisplayItem.PSize)
+		}
+
+		if pvDisplayItem.PFree != expected.PFree {
+			t.Errorf("Expected PFree %f, got %f", expected.PFree, pvDisplayItem.PFree)
+		}
+	}
 
 }
