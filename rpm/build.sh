727ef92ee1711ad6aef2d2fb3b6b005f7a4f352f
Gabriele Iannetti <g.iannetti@gsi.de>
2024-02-02 09:22:54 +0100

    Create spec file in rpm build dir

--- rpm/build.sh
+++ rpm/build.sh
@@ -24,16 +24,16 @@ function do_checks {
 
 export VERSION=$(cat VERSION)
 export BUILD_DIR=$HOME/rpmbuild
+export BUILD_SPEC=$BUILD_DIR/SPECS/prometheus-pvdisplay-exporter.spec
 export PKG_DIR=prometheus-pvdisplay-exporter-$VERSION
 
 go build
 
-sed -i "s/VERSION/$(cat VERSION)/" rpm/prometheus-pvdisplay-exporter.spec
+sed "s/VERSION/$(cat VERSION)/" rpm/prometheus-pvdisplay-exporter.spec > $BUILD_SPEC
 mkdir -p $BUILD_DIR/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 mkdir -p $BUILD_DIR/SOURCES/$PKG_DIR/usr/sbin
 mkdir -p $BUILD_DIR/SOURCES/$PKG_DIR/usr/lib/systemd/system
 mkdir -p $BUILD_DIR/SOURCES/$PKG_DIR/etc/sudoers.d
-cp rpm/prometheus-pvdisplay-exporter.spec $BUILD_DIR/SPECS/
 cp systemd/prometheus-pvdisplay-exporter.service $BUILD_DIR/SOURCES/$PKG_DIR/usr/lib/systemd/system/
 cp sudoers/prometheus-pvdisplay-exporter $BUILD_DIR/SOURCES/$PKG_DIR/etc/sudoers.d/
 cp prometheus-pvdisplay-exporter $BUILD_DIR/SOURCES/$PKG_DIR/usr/sbin/
@@ -42,5 +42,5 @@ tar -czvf $PKG_DIR.tar.gz $PKG_DIR
 cd $BUILD_DIR
 echo build dir is $BUILD_DIR
 ls -la $BUILD_DIR/SOURCES
-rpmbuild -ba $BUILD_DIR/SPECS/prometheus-pvdisplay-exporter.spec
+rpmbuild -ba $BUILD_SPEC
 
